dd471a83d371f4200cc3589a0353553e5f32f210
LMSimas <luis.simas.goncalves@tecnico.ulisboa.pt>
2020-11-17 21:47:55 +0000

    ist87058

--- software/firmware/firmware.c
+++ software/firmware/firmware.c
@@ -5,9 +5,28 @@
 
 int main()
 {
+
+int i = 0;//iterador
+int media = 0;
+int acumulador = 0;
+int valor_random = 0;//guarda valor random gerado
+int n_valores_processados = 0;
+
   //init uart 
   uart_init(UART_BASE,FREQ/BAUD);   
-  uart_printf("\n\n\nHello world!\n\n\n");
+  
+  for(i=1; i<= 100; i++){//i=1, (...), i=100
+     	valor_random = rand();//recebe 1 valor random
+     	uart_printf(" %d \n", valor_random);
+     	
+     	if(i>10 && i<91){ //assim não processamos os 10 primeiros e os 10 ultimos valores
+		acumulador += valor_random;
+		n_valores_processados++;
+	}     		
+
+     }
+     media = acumulador / n_valores_processados;
+  uart_printf("Media truncada: %d\n", media);
   //char *a = malloc(10);
   //free(a);
 }
