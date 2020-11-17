4994f75ff0f36cbc34413c28744420a019b194e4
LMSimas <luis.simas.goncalves@tecnico.ulisboa.pt>
2020-11-17 22:14:09 +0000

    ist87058 - gera valores entre 0 e 20 (no overflow this time)

--- software/firmware/firmware.c
+++ software/firmware/firmware.c
@@ -16,16 +16,15 @@ int n_valores_processados = 0;
   uart_init(UART_BASE,FREQ/BAUD);   
   
   for(i=1; i<= 100; i++){//i=1, (...), i=100
-     	valor_random = rand();//recebe 1 valor random
-     	uart_printf(" %d \n", valor_random);
+     	valor_random = rand() % 20;//recebe 1 valor random entre 0 e 20 (pra nao ocorrer overflow)
+     	//uart_printf(" %d \n", valor_random);
      	
-     	if(i>10 && i<91){ //assim não processamos os 10 primeiros e os 10 ultimos valores
-		acumulador += valor_random;
-		n_valores_processados++;
-	}     		
-
+	acumulador += valor_random;
+	n_valores_processados++;
+		
      }
      media = acumulador / n_valores_processados;
+     
   uart_printf("Media truncada: %d\n", media);
   //char *a = malloc(10);
   //free(a);
